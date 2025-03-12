93436a3c9257a247bb6080cd35ef6936a0e0c15c
dotz0ver <opir0oui@gmail.com>
2025-03-12 11:23:10 +0900

    토마토 / 골드5

---
+++ 0x09/7576.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+m, n = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs():
+    queue = deque()
+
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                queue.append((i, j))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+        
+bfs()
+
+cnt = 0
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 0:
+            print(-1)
+            exit()
+        cnt = max(cnt, graph[i][j])
+
+print(cnt - 1)
